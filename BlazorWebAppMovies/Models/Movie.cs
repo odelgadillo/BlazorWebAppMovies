45019be00b95cc53d55e297ac4bad5ae3762424a
odelgadillo <odelgadillo@outlook.com>
2025-08-18 14:25:36 -0400

    Agregar nueva clase e Incorporación de paquetes y herramientas Nuget

---
+++ BlazorWebAppMovies/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorWebAppMovies;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string? Titulo { get; set; }
+    public DateOnly FechaPublicacion { get; set; }
+    public string? Genero { get; set; }
+
+    [DataType(DataType.Currency)]
+    [Column(TypeName = "decimal(18,2)")]
+    public Decimal Precio { get; set; }
+}
