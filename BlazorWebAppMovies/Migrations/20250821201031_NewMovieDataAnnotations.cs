5061e5094aba346d7d0439ed9f0de4cbe2642cc3
odelgadillo <odelgadillo@outlook.com>
2025-08-21 16:14:20 -0400

    Agregar anotaciones de datos a la clase Pelicula y nueva migracion de base de datos

---
+++ BlazorWebAppMovies/Migrations/20250821201031_NewMovieDataAnnotations.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorWebAppMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMovieDataAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Pelicula",
+                type: "TEXT",
+                maxLength: 60,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genero",
+                table: "Pelicula",
+                type: "TEXT",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Titulo",
+                table: "Pelicula",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Genero",
+                table: "Pelicula",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 30);
+        }
+    }
+}
--- BlazorWebAppMovies/Models/Pelicula.cs
+++ BlazorWebAppMovies/Models/Pelicula.cs
@@ -6,10 +6,19 @@ namespace BlazorWebAppMovies;
 public class Pelicula
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(60, MinimumLength = 3)]
     public string? Titulo { get; set; }
+
     public DateOnly FechaPublicacion { get; set; }
+    
+    [Required]
+    [StringLength(30)]
+    [RegularExpression(@"^[A-Z]+[a-zA-Z()\s-]*$")]
     public string? Genero { get; set; }
 
+    [Range(0, 100)]
     [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18,2)")]
     public Decimal Precio { get; set; }
